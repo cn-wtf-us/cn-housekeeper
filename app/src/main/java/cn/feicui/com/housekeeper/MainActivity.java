88c79d2415a4bd093ac233057b728b678026632e
Aries <940066717@qq.com>
2016-09-07 17:43:58 +0800

    add property animator

--- app/src/main/java/cn/feicui/com/housekeeper/MainActivity.java
+++ app/src/main/java/cn/feicui/com/housekeeper/MainActivity.java
@@ -1,7 +1,10 @@
 package cn.feicui.com.housekeeper;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView iv_hzw = (ImageView) findViewById(R.id.iv_hzw);
+
+        AnimatorSet animator = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.animator_alpha);
+        animator.setTarget(iv_hzw);
+        animator.start();
     }
 }
